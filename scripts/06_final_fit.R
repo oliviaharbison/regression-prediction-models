3b688f7d113c347e2997cc7a336d0c7652f18dfa
oliviaharbison <oliviaharbison2024@u.northwestern.edu>
2024-03-09 14:03:06 -0600

    final predictions plot

--- scripts/06_final_fit.R
+++ scripts/06_final_fit.R
@@ -13,11 +13,13 @@ doMC::registerDoMC(cores = parallel::detectCores(logical = TRUE))
 # data
 load(here("data/music_split.rda"))
 load(here("results/rf_tuned.rda"))
+load(here("results/rf_tuned_2.rda"))
 
 # finalize workflow ---
 final_wflow <- rf_tuned %>%
   extract_workflow(rf_tuned) %>%  
   finalize_workflow(select_best(rf_tuned, metric = "rmse"))
+
 
 # train final model ---
 final_fit <- fit(final_wflow, music_train)
@@ -26,8 +28,7 @@ final_fit <- fit(final_wflow, music_train)
 save(final_fit, file = here("results/final_fit.rda"))
 
 
-#After fitting/training the best model in the last task, assess the model's 
-#performance on the test set
+# assess performance ----
 
 final_preds <- music_test %>%
   select(popularity) %>%
@@ -38,12 +39,14 @@ metrics <- metric_set(rmse, mae, rsq)
 final_mets <- metrics(final_preds, truth = popularity, estimate = .pred)
 
 # save
+save(final_preds, file = here("results/final_preds.rda"))
 save(final_mets, file = here("results/final_mets.rda"))
 
-# plot
+
+# plot ----
 results_plot <- ggplot(final_preds, aes(x = popularity, y = .pred)) +
   geom_abline(lty = 2) +
-  geom_point(alpha = 0.7) +
+  geom_point(alpha = 0.5) +
   labs(y = "Predicted Popularity", x = "Actual Popularity") +
   coord_obs_pred() +
   theme_minimal()
